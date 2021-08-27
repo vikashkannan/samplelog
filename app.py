93058d167993a9ae26469f61343792f4ce031462
vikashkannan <54845189+vikashkannan@users.noreply.github.com>
2021-08-27 14:54:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,6 @@
-import log_helper
-from test_log import test_logger
-from test_log_01 import test_logger_01
-import os
-
-from flask import Flask, render_template
-
-ASSETS_DIR = os.path.dirname(os.path.abspath(__file__))
+from flask import Flask
 app = Flask(__name__)
 
-log = log_helper.get_logger(__name__)
-log_helper.log_setup()
-
-
-def main():
-    log.info('Test Info')
-    test_logger()
-    pass
-
-
-@app.route('/', methods=['GET'])
-def index():
-    test_logger()
-    return "Welcome to Azure"
-
-
-@app.route('/data')
-def names():
-    test_logger()
-    data = {"names": ["John", "Jacob", "Julie", "Jennifer"]}
-    return data
+@app.route("/")
+def hello():
+    return "Hello, World!"
