6ce621cfc46075e974a5f17d92129f3fef2922d8
vikashkannan <54845189+vikashkannan@users.noreply.github.com>
2021-08-27 17:20:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,11 @@ import log_helper
 app = Flask(__name__)
 log = log_helper.get_logger(__name__)
 
-
 @app.route("/")
 def index():
+    log_helper.log_setup()
+    log.debug('Test Log Debug')
+    log.info('Test Log Info')
+    log.warning('Test Log Warning')
+    log.error('Test Log Error')
     return "Hello, Azure World"
